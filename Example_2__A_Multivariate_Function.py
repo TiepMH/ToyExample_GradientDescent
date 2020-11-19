859049d4872aba5bf9fd0e8c687655a0d495c9f2
Tiep M. H <50709837+TiepMH@users.noreply.github.com>
2020-11-19 21:24:10 +0000

    Update Example_2__A_Multivariate_Function.py

--- Example_2__A_Multivariate_Function.py
+++ Example_2__A_Multivariate_Function.py
@@ -1,7 +1,9 @@
+''' Tiep M. Hoang '''
+###############################################################################
+
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
 
 ###############################################################################
 ''' Tensorflow '''
